26f947d1664e3b74aca24c42d8eb8d5e23f78c5b
Alex <a.chisar00@gmail.com>
2021-06-09 16:51:13 -0400

    comment model added

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { User, Blog } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'blog_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name']
+                    }
+                },
+            ],
+        });
+
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('dashboard', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
